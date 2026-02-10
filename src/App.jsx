b2150ee7a8901dacfcc2daa3a1879b29dec2d519
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-10 13:52:40 +0100

    git init

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+const articles = ['Titolo 1', 'Titolo 2', 'Titolo 3'];
+console.log(articles);
+
+export default function App() {
+  return (
+    <>
+      <div className="container text-center bg-white border border-dark ">
+        {articles.map((article, index) => {
+          return (
+            <ul>
+              <li>{article}</li>
+            </ul>
+          );
+        })}
+      </div>
+
+      <form action="">
+        <div>
+          <label htmlFor="InputTitle">Inserisci Titolo articolo</label>
+        </div>
+      </form>
+    </>
+  );
+}
